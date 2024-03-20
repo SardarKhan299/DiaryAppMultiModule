b093c69e32e40c9452f960241a9038df4c9166ff
SardarkhanStella <skhan@stellatechnology.com>
2024-03-20 15:30:32 +0500

    Fix gradle issues.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("kotlin-kapt")
-    id("io.realm.kotlin")
+    id ("com.google.devtools.ksp")
     id("dagger.hilt.android.plugin")
+    id("io.realm.kotlin")
     //id("com.google.gms.google-services")
 }
 
@@ -44,7 +44,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.4.6"
     }
     packaging {
         resources {
@@ -54,80 +54,69 @@ android {
 }
 
 dependencies {
+    val compose_version : String by rootProject.extra
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
+    implementation ("androidx.core:core-ktx:1.12.0")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation ("androidx.activity:activity-compose:1.7.2")
+    implementation ("androidx.compose.ui:ui:$compose_version")
+    implementation ("androidx.compose.ui:ui-tooling-preview:$compose_version")
+    implementation ("androidx.compose.material3:material3:1.2.0-alpha08")
+    testImplementation ("junit:junit:4.13.2")
+    androidTestImplementation ("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation ("androidx.compose.ui:ui-test-junit4:1.5.1")
+    debugImplementation ("androidx.compose.ui:ui-tooling:$compose_version")
+    debugImplementation ("androidx.compose.ui:ui-test-manifest:$compose_version")
 
+    // Compose Navigation
+    implementation ("androidx.navigation:navigation-compose:2.7.3")
 
-    // compose navigation...//
-    implementation("androidx.navigation:navigation-compose:2.7.7")
+    // Firebase
+    implementation ("com.google.firebase:firebase-auth-ktx:22.1.2")
+    implementation ("com.google.firebase:firebase-storage-ktx:20.2.1")
 
-    // Firebase...//
-    implementation("com.google.firebase:firebase-auth-ktx:22.3.1")
-    implementation("com.google.firebase:firebase-storage-ktx:20.2.1")
+    // Room components
+    implementation ("androidx.room:room-runtime:2.5.2")
+    ksp ("androidx.room:room-compiler:2.5.2")
+    implementation ("androidx.room:room-ktx:2.5.2")
 
+    // Runtime Compose
+    implementation ("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
 
-    // Room Components...//
-    implementation("androidx.room:room-runtime:2.6.1")
-    kapt("androidx.room:room-compiler:2.6.1")
-    implementation("androidx.room:room-ktx:2.6.1")
+    // Splash API
+    implementation ("androidx.core:core-splashscreen:1.0.1")
 
-    // runtime compose...//
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
+    // Mongo DB Realm
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
+    implementation ("io.realm.kotlin:library-sync:1.11.0")
 
-    // splash Api..//
-    implementation("androidx.core:core-splashscreen:1.0.1")
+    // Dagger Hilt
+    implementation ("com.google.dagger:hilt-android:2.48")
+    ksp ("com.google.dagger:hilt-compiler:2.48")
+    implementation ("androidx.hilt:hilt-navigation-compose:1.0.0")
 
-    // Mongo DB Realm...//
-//    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0-native-mt"){
-//        version{
-//            strictly("1.5.0-native-mt")
-//        }
-//    }
-//    implementation("io.realm.kotlin:library-sync:1.0.2")
+    // Coil
+    implementation ("io.coil-kt:coil-compose:2.3.0")
 
-    implementation ("io.realm.kotlin:library-base:1.11.0")
-    implementation ("io.realm.kotlin:library-sync:1.11.0") // If using Device Sync
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.0") // If using coroutines with the SDK
+    // Pager - Accompanist [DEPRECATED]
+//    implementation ("com.google.accompanist:accompanist-pager:0.27.0")
 
+    // Date-Time Picker
+    implementation ("com.maxkeppeler.sheets-compose-dialogs:core:1.0.2")
 
+    // CALENDAR
+    implementation ("com.maxkeppeler.sheets-compose-dialogs:calendar:1.0.2")
 
-// Dagger Hilt...//
-    implementation("com.google.dagger:hilt-android:2.44.2")
-    kapt("com.google.dagger:hilt-compiler:2.44.2")
-    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+    // CLOCK
+    implementation ("com.maxkeppeler.sheets-compose-dialogs:clock:1.0.2")
 
+    // Message Bar Compose
+    implementation ("com.github.stevdza-san:MessageBarCompose:1.0.5")
 
-    // Google Auth...//
-    //implementation("com.google.android.gms.play-service-auth:20.4.0")
+    // One-Tap Compose
+    implementation ("com.github.stevdza-san:OneTapCompose:1.0.7")
 
-    /// Coil...//
-    implementation("io.coil-kt:coil-compose:2.2.2")
-
-    //Pager --- Accompanist..//
-    implementation("com.google.accompanist:accompanist-pager:0.27.0")
-
-    // Date Time Picker...//
-    implementation("io.github.vanpra.compose-material-dialogs:datetime:0.9.0")
-
-    // Message bar Compose...//
-    implementation("com.github.stevdza-san:OneTapCompose:1.0.0")
-
-    // Desugar JDK...//
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
-
-
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    // Desugar JDK
+    coreLibraryDesugaring ("com.android.tools:desugar_jdk_libs:2.0.3")
 }
