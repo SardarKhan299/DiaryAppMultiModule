cdd074e7c19395b47b829ef6ff753009a2885612
SardarkhanStella <skhan@stellatechnology.com>
2024-03-28 15:22:07 +0500

    Add Diary Header and Complete Diary Holder Component.

--- app/src/main/java/com/example/diaryappmultimodule/presentation/components/DiaryHolder.kt
+++ app/src/main/java/com/example/diaryappmultimodule/presentation/components/DiaryHolder.kt
@@ -1,61 +1,122 @@
 package com.example.diaryappmultimodule.presentation.components
 
 import android.icu.util.LocaleData
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.diaryappmultimodule.model.Diary
+import com.example.diaryappmultimodule.model.Mood
+import com.example.diaryappmultimodule.ui.theme.Elevation
+import com.example.diaryappmultimodule.util.toInstant
+import java.time.Instant
 import java.time.LocalDate
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 @Composable
-    fun DateHeader(localDate: LocalDate) {
-        Row (verticalAlignment = Alignment.CenterVertically){
-            Column(horizontalAlignment = Alignment.End) {
-                Text(text = String.format("%02d",localDate.dayOfMonth),
-                    style = TextStyle(
-                        fontSize = MaterialTheme.typography.titleLarge.fontSize,
-                        fontWeight = FontWeight.Light
-                    )
-                )
-                Text(text = "${localDate.dayOfWeek.toString().take(3)}",
-                    style = TextStyle(
-                        fontSize = MaterialTheme.typography.bodySmall.fontSize,
-                        fontWeight = FontWeight.Light
-                    )
-                )
-            }
+fun DiaryHolder(diary: Diary, onclick:(String)-> Unit) {
+    var localDensity = LocalDensity.current
+    var componentHeight by remember { mutableStateOf(0.dp) }
+    Row (modifier = Modifier.clickable(indication = null, interactionSource = remember { MutableInteractionSource() }) {
+        onclick(diary._id.toString()) }){
+        Spacer(modifier = Modifier.width(14.dp))
+        Surface(modifier = Modifier
+            .width(2.dp)
+            .height(14.dp), tonalElevation = Elevation.Level1) {
 
-            Spacer(modifier = Modifier.width(14.dp))
-
-            Column(horizontalAlignment = Alignment.Start) {
-                Text(text = localDate.month.toString().lowercase().replaceFirstChar { it.titlecase() },
-                    style = TextStyle(
-                        fontSize = MaterialTheme.typography.titleLarge.fontSize,
-                        fontWeight = FontWeight.Light
-                    )
-                )
-                Text(text = "${localDate.year}",
-                    color = MaterialTheme.colorScheme.onSurface.copy(0.38f),
-                    style = TextStyle(
-                        fontSize = MaterialTheme.typography.bodySmall.fontSize,
-                        fontWeight = FontWeight.Light
-                    )
+        }
+        Spacer(modifier = Modifier.width(20.dp))
+        Surface(modifier = Modifier
+            .clip(shape = Shapes().medium)
+            .onGloballyPositioned {
+                componentHeight = with(localDensity) { it.size.height.toDp() }
+            }, tonalElevation = Elevation.Level1) {
+            Column(modifier = Modifier.fillMaxWidth()) {
+                DiaryHeader(moodName = diary.mood, time = diary.date.toInstant())
+                Text(modifier = Modifier.padding(14.dp), text = diary.description,
+                    style = TextStyle(fontSize = MaterialTheme.typography.bodyLarge.fontSize),
+                    maxLines = 4,
+                    overflow = TextOverflow.Ellipsis
                 )
             }
         }
-    }
 
-@Preview(showBackground = true)
+    }
+}
+
 @Composable
-fun DateHeaderPreview() {
-    DateHeader(localDate = LocalDate.now())
+fun DiaryHeader(moodName: String, time: Instant) {
+    val mood by remember { mutableStateOf(Mood.valueOf(moodName)) }
+    val formatter = remember {
+        DateTimeFormatter.ofPattern("hh:mm a", Locale.getDefault())
+            .withZone(ZoneId.systemDefault())
+    }
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(mood.containerColor)
+            .padding(horizontal = 14.dp, vertical = 7.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Image(
+                modifier = Modifier.size(18.dp),
+                painter = painterResource(id = mood.icon),
+                contentDescription = "Mood Icon",
+            )
+            Spacer(modifier = Modifier.width(7.dp))
+            Text(
+                text = mood.name,
+                color = mood.contentColor,
+                style = TextStyle(fontSize = MaterialTheme.typography.bodyMedium.fontSize)
+            )
+        }
+        Text(
+            text = formatter.format(time),
+            color = mood.contentColor,
+            style = TextStyle(fontSize = MaterialTheme.typography.bodyMedium.fontSize)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun DiaryPrev() {
+    DiaryHolder(diary = Diary().apply { title = "My Diary"
+                                      description = "My diary Description"
+                                      mood = Mood.Calm.name
+                                      }, onclick = {})
 }
