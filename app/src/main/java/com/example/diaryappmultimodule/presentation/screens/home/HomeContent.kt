4f6762fb58b8bf054fc5b33ac1231bc39c3b6ee1
SardarkhanStella <skhan@stellatechnology.com>
2024-04-01 14:44:47 +0500

    Add home content and home screen and also implement lazyList with data.

--- app/src/main/java/com/example/diaryappmultimodule/presentation/screens/home/HomeContent.kt
+++ app/src/main/java/com/example/diaryappmultimodule/presentation/screens/home/HomeContent.kt
@@ -1,9 +1,15 @@
 package com.example.diaryappmultimodule.presentation.screens.home
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,6 +19,8 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.diaryappmultimodule.model.Diary
+import com.example.diaryappmultimodule.presentation.components.DiaryHolder
 import java.time.LocalDate
 
 @Composable
@@ -50,6 +58,57 @@ fun DateHeader(localDate: LocalDate) {
                 )
             )
         }
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun HomeContent(diaryNotes:Map<LocalDate,List<Diary>>,
+                onclick: (String)->Unit) {
+
+    if(diaryNotes.isNotEmpty()){
+        LazyColumn(modifier = Modifier.padding(horizontal = 24.dp)){
+            diaryNotes.forEach{ (localDate,diaries)->
+                stickyHeader(key = localDate){
+                    DateHeader(localDate = localDate)
+                }
+                items(items = diaries, key = { it._id }){
+                    DiaryHolder(diary = it, onclick = onclick)
+                }
+            }
+        }
+    }else{
+        EmptyPage()
+    }
+
+}
+
+@Composable
+fun EmptyPage(
+    title: String = "Empty Diary",
+    subtitle: String = "Write Something"
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(all = 24.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = title,
+            style = TextStyle(
+                fontSize = MaterialTheme.typography.titleMedium.fontSize,
+                fontWeight = FontWeight.Medium
+            )
+        )
+        Text(
+            text = subtitle,
+            style = TextStyle(
+                fontSize = MaterialTheme.typography.bodyMedium.fontSize,
+                fontWeight = FontWeight.Normal
+            )
+        )
     }
 }
 
--- app/src/main/java/com/example/diaryappmultimodule/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/diaryappmultimodule/presentation/screens/home/HomeScreen.kt
@@ -1,15 +1,21 @@
 package com.example.diaryappmultimodule.presentation.screens.home
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Edit
@@ -27,8 +33,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.example.diaryappmultimodule.R
+import com.example.diaryappmultimodule.model.Diary
+import com.example.diaryappmultimodule.presentation.components.DiaryHolder
+import java.time.LocalDate
+
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -44,12 +57,14 @@ fun HomeScreen(drawerState: DrawerState,onMenuClicked: ()-> Unit,
                     contentDescription ="New Diary Icon" )
             }
         }, content = {
-
+            HomeContent(diaryNotes = mapOf() , onclick = {})
         })
         
     }
 
 }
+
+
 
 @Composable
 fun NavigationDrawer(drawerState: DrawerState,onSignOutClicked: ()->Unit,
